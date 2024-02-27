2f68e70c66152a2ea7859506993894f542fb42b0
RoronuaHong <807672412@qq.com>
2024-02-28 04:33:12 +0800

    completed blocks

--- vue-lesson/src/packages/MyEditor.jsx
+++ vue-lesson/src/packages/MyEditor.jsx
@@ -1,5 +1,6 @@
-import { inject, computed, defineComponent } from 'vue'
+import { ref, inject, computed, onMounted, defineComponent } from 'vue'
 import MyEditorBlock from './MyEditorBlock'
+import deepcopy from 'deepcopy'
 
 import './editor.scss'
 
@@ -9,10 +10,14 @@ export default defineComponent({
       type: Object
     }
   },
-  setup(props) {
+  emits: ['update:modelValue'],      // 要触发的事件
+  setup(props, ctx) {
     const data = computed({
       get() {
         return props.modelValue
+      },
+      set(newVal) {
+        ctx.emit('update:modelValue', deepcopy(newVal))
       }
     })
 
@@ -23,10 +28,71 @@ export default defineComponent({
 
     const config = inject('config')
 
+    let currentComponent = null
+    const containerRef = ref(null)
+
+    const dragenter = (e) => {
+      e.dataTransfer.dropEffect = 'move'
+    }
+
+    const dragover = (e) => {
+      e.preventDefault()
+    }
+
+    const dragleave = (e) => {
+      e.dataTransfer.dropEffect = 'none'
+    }
+
+    const drop = (e) => {
+      // 先留在这
+      console.log(e)
+      console.log(currentComponent)
+
+      let blocks = data.value.blocks
+
+      data.value = {
+        ...data.value,
+        blocks: [
+          ...blocks,
+          {
+            top: e.offsetY,
+            left: e.offsetX,
+            zIndex: 1,
+            key: currentComponent.key,
+            alignCenter: true,
+          }
+        ]
+      }
+
+      currentComponent = null
+    }
+
+    const dragstart = (e, component) => {
+      console.log(e, component)
+      console.log(containerRef.value)
+
+      currentComponent = component
+    }
+
+    onMounted(() => {
+      // dragenter 进入元素中, 添加一个移动标识
+      // dragover 在目标元素经过, 必须阻止默认行为, 否则不能触发drop
+      // dragleave 离开元素的时候, 需要增加一个禁用标识
+      // drop 松手的时候, 根据拖拽的组件, 添加一个组件
+      containerRef.value.addEventListener('dragenter', dragenter)
+      containerRef.value.addEventListener('dragover', dragover)
+      containerRef.value.addEventListener('dragleave', dragleave)
+      containerRef.value.addEventListener('drop', drop)
+    })
+
     return () => <div class="editor">
       <div class="editor-left">
+        {/* 根据注册列表 渲染对应的内容 可以实现h5拖拽 */}
         {config.componentList.map(component => (
-          <div class="editor-left-item">
+          <div class="editor-left-item"
+            draggable
+            onDragstart={e => dragstart(e, component)}
+          >
             <span>{component.label}</span>
             <div>{component.preview()}</div>
           </div>
@@ -38,7 +104,7 @@ export default defineComponent({
         {/* 负责产生滚动条 */}
         <div class="editor-container-canvas">
           {/* 产生内容区域 */}
-          <div class="editor-container-canvas__content" style={containerStyles.value}>
+          <div class="editor-container-canvas__content" style={containerStyles.value} ref={containerRef}>
             { (data.value.blocks).map(block => (
               <MyEditorBlock block={block} />
             )) }
--- vue-lesson/src/packages/MyEditorBlock.jsx
+++ vue-lesson/src/packages/MyEditorBlock.jsx
@@ -1,21 +1,57 @@
-import { inject, computed, defineComponent } from 'vue'
+import { ref, inject, computed, onMounted, defineComponent } from 'vue'
+// import deepcopy from 'deepcopy'
 
 export default defineComponent({
   props: {
     block: {
-      type: Object
+      type: Object,
+      required: true
     }
   },
-  setup(props) {
-    const blockStyles = computed(() => ({
+  emits: ['update:block'], // 声明自定义事件
+  setup(props, ctx) {
+    const style = {
       top: `${props.block.top}px`,
       left: `${props.block.left}px`,
       zIndex: `${props.block.zIndex}`
+    }
+
+    // const blockStyles = computed(() => style)
+
+    const blockStyles = computed(() => ({
+      get:() => style,
+      set: (newVal) => {
+        ctx.emit('update:block', newVal)
+      }
     }))
 
     const config = inject('config')
-    
-    console.log(config)
+    const blockRef = ref(null)
+
+    onMounted(() => {
+      let { offsetWidth, offsetHeight } = blockRef.value
+
+      console.log(offsetWidth, offsetHeight)
+
+      // 说明是拖拽松手的时候才渲染的, 其他的默认渲染到页面上的内容不需要居中
+      if(props.block.alignCenter) {
+        const newLeft = props.block.left - offsetWidth / 2
+        const newTop = props.block.top - offsetHeight / 2  // 原则上重新派发事件
+        const newAlignCenter = false
+
+        blockStyles.value = {
+          ...props.block.block,
+          left: newLeft,
+          top: newTop,
+          alignCenter: newAlignCenter
+        }
+
+        console.log(111)
+        console.log(blockStyles.value)
+
+        ctx.emit('update:block', blockStyles.value)
+      }
+    })
 
     return () => {
       // 通过block的key属性直接获取对应的组件
@@ -25,7 +61,7 @@ export default defineComponent({
       const RenderComponent = component.render()
 
       return (
-        <div class="editor-block" style={blockStyles.value}>
+        <div class="editor-block" style={blockStyles.value.get()} ref={blockRef}>
           {RenderComponent}
         </div>
       )
